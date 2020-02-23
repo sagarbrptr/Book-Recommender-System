83ad7e0c9988f061c93b620acde89e78cd497639
agrawaltejas01 <agrawaltejas01@gmail.com>
2020-02-23 20:25:36 +0530

    Azure settings added, ratings class created in student model

--- BRS/student/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    # Blank indicates that this is homepage of this app
-    # /word_finder
-    url(r'^$', views.index, name = "index"),   
-]
--- BRS/student/views.py
+++ BRS/student/views.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.http import HttpResponse
+
 from django.shortcuts import render
 
-def index(request):
-    return HttpResponse("<h1> Student Homepage </h1>")
-
+# Create your views here.
