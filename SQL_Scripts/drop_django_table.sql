f63b50fc227582525c38dcdbf9981616489d37dd
agrawaltejas01 <agrawaltejas01@gmail.com>
2020-03-21 11:21:13 +0530

    SQL Scripts added

---
+++ SQL_Scripts/drop_django_table.sql
@@ -0,0 +1,10 @@
+drop table django_session;
+drop table django_migrations;
+drop table django_admin_log;
+drop table auth_user_user_permissions;
+drop table auth_user_groups;
+drop table auth_group_permissions;
+drop table auth_user;
+drop table auth_group;
+drop table auth_permission;
+drop table django_content_type;
