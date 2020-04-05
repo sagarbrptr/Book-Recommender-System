4a7b5b5898e81b41b46df91f91baf1ee06a16cea
agrawaltejas01 <agrawaltejas01@gmail.com>
2020-04-05 12:39:11 +0530

    VsCode env created

--- BRS/librarian/views.py
+++ BRS/librarian/views.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.db import connection, transaction
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
-import mysql.connector
+# import mysql.connector
 from django.template import RequestContext
 from student.models import *
 
